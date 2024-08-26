94c944d886ee40b9ce02fda1073d7e40ed899aec
Trevor Gituru <giturutrevor@gmail.com>
2024-08-26 17:59:53 +0300

    Task 0: How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -27,13 +27,12 @@ def number_of_subscribers(subreddit):
         int: Number of subscribers or 0 if the subreddit is invalid.
     """
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json?raw_json=1"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if (response.status_code == 200):
-        nos_subscribers = response.json().get('data').get('subscribers')
-    else:
-        nos_subscribers = 0
-    return nos_subscribers
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")

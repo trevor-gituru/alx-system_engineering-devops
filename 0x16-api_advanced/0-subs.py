4d93f3a107102e0858793ec65941c3aff915009c
Trevor Gituru <giturutrevor@gmail.com>
2024-08-28 17:49:12 +0300

    Task 0: How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and returns the number of subscribers
-for a given subreddit"""
-
-import requests
+"""
+Query Reddit API for number of subscribers for a given subreddit
+"""
+from json import loads
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API and returns the number of
-    subscribers for a given subreddit"""
+    """ecursive function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit. If no
+    results are found for the given subreddit, the function should return None
+    """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('data').get('subscribers')
-    else:
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    response = get(url, headers=headers)
+    reddits = response.json()
+
+    try:
+        subscribers = reddits.get('data').get('subscribers')
+        return int(subscribers)
+    except:
         return 0
